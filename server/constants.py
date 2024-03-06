1d0d0589a614c5e4aa0ca92f75e03888403502f2
Tristan Batchler <tbatchler@symplicity.com>
2024-03-06 11:59:04 +1000

    Remove unneccessary constants

--- server/constants.py
+++ server/constants.py
@@ -1,6 +1,1 @@
-from typing import Literal
-tb_params: tuple[int, Literal["big", "little"]] = (16, "big")
-
-EVERYONE: bytes = (0).to_bytes(*tb_params)
-ONLY_CLIENT: bytes = (1).to_bytes(*tb_params)
-ONLY_PROTO: bytes = (2).to_bytes(*tb_params)
+EVERYONE: bytes = (0).to_bytes(16, "big")
