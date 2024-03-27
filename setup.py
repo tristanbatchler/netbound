562086f77f4550b5840be95cf9ae870e4fc95720
Tristan Batchler <tbatchler@symplicity.com>
2024-03-27 14:32:08 +1000

    Oops

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ dependencies = (this_directory / "requirements.txt").read_text().splitlines()
 
 setup(
     name='netbound',
-    version='0.1.10',
+    version='0.1.11',
     packages=find_packages(),
     url='https://github.com/tristanbatchler/netbound',
     install_requires=dependencies,
